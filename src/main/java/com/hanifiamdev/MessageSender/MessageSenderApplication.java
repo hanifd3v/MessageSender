1da4cdfb2c87ff7f25ec665699e26e809b82ea16
hanif.amrullah <hanif.amrullah@sigma.co.id>
2025-03-30 16:56:14 +0700

    init project

---
+++ src/main/java/com/hanifiamdev/MessageSender/MessageSenderApplication.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.MessageSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSenderApplication.class, args);
+    }
+
+}
